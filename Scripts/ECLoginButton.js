69642997bf46bbcb9f28bb5f383cbb803e9e3885
Awesomeness3301 <80080270+Awesomeness3301@users.noreply.github.com>
2025-09-24 01:59:29 -0400

    Update ECLoginButton.js

--- Scripts/ECLoginButton.js
+++ Scripts/ECLoginButton.js
@@ -7,17 +7,12 @@ var wrongCounter = 0;
     if (user.value == "1967EthicsCommittee" && pass.value != "") {
       location.href =
         "https://sites.google.com/view/bksntheethicscommitteehomepage/home";
-    } else if (user.value == "Secret" && pass.value == "Password") {
-      window.open("funnywebsite.html", "_blank")
     } else {
       wrongCounter = wrongCounter + 1;
       wrongMsg.style.visibility = "visible";
       user.value = ""
       pass.value = ""
     }
-
-    if (wrongCounter == 3) {
-      location.href = "accessdenied.html";
-    }
-
   }
+
+
